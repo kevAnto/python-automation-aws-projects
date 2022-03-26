6bc3619906d770ab38f1b4726d2a6d044b9782e0
kevanto <antoine.choula@gmail.com>
2022-03-26 14:13:59 +0100

    init

---
+++ restore-volume.py
@@ -0,0 +1,52 @@
+import boto3
+from operator import itemgetter
+
+ec2_client = boto3.client('ec2', region_name="us-east-1")
+ec2_resource = boto3.resource('ec2', region_name="us-east-1")
+
+instance_id = "i-0cced4fd9fba6d923"
+
+volumes = ec2_client.describe_volumes(
+    Filters=[
+        {
+            'Name': 'attachment.instance-id',
+            'Values': [instance_id]
+        }
+    ]
+)
+
+instance_volume = volumes['Volumes'][0]
+
+snapshots = ec2_client.describe_snapshots(
+    OwnerIds=['self'],
+    Filters=[
+            {
+                'Name': 'volume-id',
+                'Values': [instance_volume['VolumeId']]
+            }
+        ]
+    )
+
+latest_snapshot = sorted(snapshots['Snapshots'], key=itemgetter('StartTime'), reverse=True)[0]
+print(latest_snapshot['StartTime'])
+
+new_volume = ec2_client.create_volume(
+    SnapshortId=latest_snapshot['SnapshotId'],
+    AvailabilityZone="us-east-1c",
+    TagSpecifications=[
+        {
+            'ResourceType': 'volume',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': 'prod'
+                }
+            ]
+        }
+    ]
+)
+
+ec2_resource.Instance(instance_id).attach_volume(
+    VolumeId=new_volume['VolumeId'],
+    Device='/dev/xvdb'
+)
